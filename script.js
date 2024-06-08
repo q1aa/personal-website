670a6495b0ea083021634b833c71e0cb82fef067
hmhmmhmhuh <149719174+q1aa@users.noreply.github.com>
2024-06-08 12:28:37 +0200

    init()

---
+++ script.js
@@ -0,0 +1,67 @@
+const projects = [
+    {
+        name: "Julin.dev",
+        description: "My personal website",
+        tech: ["HTML", "CSS", "JavaScript"],
+        link: "https://julin.dev"
+    },
+    {
+        name: "CSGrok",
+        description: "HTTP tunnel for sharing localhost websites to the public",
+        tech: ["C#", "ASP.NET", "HTML", "CSS", "JavaScript"],
+        link: "https://csgrok.com"
+    },
+    {
+        name: "SongLinker",
+        description: "Create and share a universal song link easily for all music platforms",
+        tech: ["TS", "Express", "HTML", "CSS", "JavaScript", "MongoDB"],
+        link: "https://songlinker.net"
+    }
+]
+
+const projectsContainer = document.querySelector(".projects");
+
+projects.forEach(project => {
+    const projectHTML = generateProjectHTML(project);
+    projectsContainer.innerHTML += projectHTML;
+});
+
+function generateProjectHTML(project) {
+    return `
+            <div class="project">
+                <div class="project-text-holder">
+                    <p class="bracket">{</p>
+                    <div class="json-content">
+                        <p class="key">"name": <span class="value">"${project.name}",</span></p>
+                        <p class="key">"description": <span class="value">"${project.description}"</span>,</p>
+                        <p class="key">"tech": <span class="value">${project.tech},</span></p>
+                        <p class="key">"link": <a href="${project.link}"><span class="value">"${project.link}"</span></a></p>
+                    </div>
+                    <p class="bracket">}</p>
+                </div>
+            </div>
+    `
+}
+
+function focusElement(className) {
+    const element = document.querySelector(`.${className}`);
+    element.scrollIntoView({ behavior: "smooth" });
+    element.style.borderRadius = "5px";
+    let opacity = 1;
+    let timer = setInterval(() => {
+        if (opacity <= 0) {
+            clearInterval(timer);
+            element.style.border = "none";
+        } else {
+            let red = Math.floor(opacity * 255);
+            let blue = 255 - red;
+            element.style.border = `2px solid rgba(${blue}, 0, ${red}, ${opacity})`;
+            opacity -= 0.02;
+        }
+    }, 50);
+}
+
+function sendMail() {
+    window.open("mailto:jul1n@icloud.com", "_blank");
+    navigator.clipboard.writeText("jul1n@icloud.com");
+}
